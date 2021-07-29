0fb7378a6ddce0be85d021aead1fde100fdf5f4d
Bheemireddy Vamsi <51514362+vamsireddy22@users.noreply.github.com>
2021-07-29 20:13:24 +0530

    Update MultiLevelInheritance.py

--- MultiLevelInheritance.py
+++ MultiLevelInheritance.py
@@ -1,31 +1,54 @@
-
+# MultiLevel Inheritance
 class A:
-    def __init__(self):
-        print("Objects created at A")
+    def __init__(self,a,b,c):
+        print("Class A Constructor")
+        self.a = a
+        self.b = b
+        self.c = c
+        print("Exiting Class A")
+
+    def PrintA(self):
+        print(self.a)
+        print(self.b)
+        print(self.c)
     
-    def DisplayA(self):
-        print("Printing at A",self)
-    
+
 class B(A):
-    def __init__(self):
-        super().__init__()
-        print("Objects created at B")
+    def __init__(self,a,b,c,d,e,f):
+        print("Class B Constructor")
+        #A.__init__(self,a,b,c)
+
+        # Super is used to call base class properties and functionalities
+        super().__init__(a,b,c)
+        self.d = d
+        self.e = e
+        self.f = f
+        print("Exiting Class B")
     
-    def DisplayB(self):
-        print("Printing at B")
+    def printB(self):
+        super().PrintA()
+        print(self.d)
+        print(self.e)
+        print(self.f)
+
 
 class C(B):
-    def __init__(self):
-        super().__init__()
-        print("Object created at C")
+    def __init__(self,a,b,c,d,e,f,g,h,i):
+        print("Class C Constructor")
+        super().__init__(a,b,c,d,e,f)
+        self.g = g
+        self.h = h
+        self.i = i
+        print("Exiting Class C")
+    
+    def printC(self):
+        super().printB()
+        print(self.g)
+        print(self.h)
+        print(self.i)
 
-    def DisplayC(self):
-        A.DisplayA(self)
-        B.DisplayB(self)
-        # B.DisplayA(self)
-        print("Printing at C")
 
-objectc= C()
-objectc.DisplayC()
-objectA = A()
-objectA.DisplayA()
+objC = C(10,20,30,40,50,60,70,80,90)
+objC.printC()
+
+        
